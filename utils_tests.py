83af32cc6b63633fc535d28d12579386b15ccd15
TakenouchiTR <draconianlds@gmail.com>
2021-11-10 17:22:18 -0500

    Implement positive tests

--- utils_tests.py
+++ utils_tests.py
@@ -17,28 +17,47 @@ class TestScan(unittest.TestCase):
         """
         Tests if a single, open port is correctly detected
         """
-        pass
+        result = utils.scan("127.0.0.1", 10000)
+
+        self.assertTrue(result[10000])
         
     def test_single_closed_port(self):
         """
         Tests if a single, closed port is correctly detected
         """
-        pass
+        result = utils.scan("127.0.0.1", 10003)
+
+        self.assertFalse(result[10003])
 
     def test_many_open_ports(self):
         """
         Tests if many open ports are correctly detected
         """
-        pass
+        result = utils.scan("127.0.0.1", 10000, 10002)
+
+        self.assertTrue(result[10000])
+        self.assertTrue(result[10001])
+        self.assertTrue(result[10002])
     
     def test_many_closed_ports(self):
         """
         Tests is many closed ports are correctly detected
         """
-        pass
+        result = utils.scan("127.0.0.1", 10003, 10005)
+
+        self.assertFalse(result[10003])
+        self.assertFalse(result[10004])
+        self.assertFalse(result[10005])
 
     def test_many_open_and_closed_ports(self):
         """
         Tests if both open and closed ports are correctly detected
         """
-        pass
+        result = utils.scan("127.0.0.1", 10000, 10005)
+
+        self.assertTrue(result[10000])
+        self.assertTrue(result[10001])
+        self.assertTrue(result[10002])
+        self.assertFalse(result[10003])
+        self.assertFalse(result[10004])
+        self.assertFalse(result[10005])
